a29837e15b68dc7b08043d3cd5c593aa75309a13
Daniil Volkov <daniil.volkov.02@yandex.ru>
2025-06-13 19:35:01 +0700

    main added

---
+++ main.py
@@ -0,0 +1,23 @@
+import gymnasium as gym
+from hesp import HESP
+from constants import MODE
+from utils import run_with_saved_weights
+
+
+# Запуск алгоритма
+if __name__ == "__main__":
+    if MODE == 'train':
+        env = gym.make("LunarLander-v3")
+        hesp = HESP(
+            env,
+            population_size=50,  # Популяция
+            hidden_units=10,     # Количество нейронов на скрытом слое
+            L1_size=20,          # Количество входных нейронов
+            L2_size=300,         # Количество выходных нейронов
+        )
+        hesp.evolve(generations=100000)
+        hesp.save_best_network("best_network_weights.json")
+        env.close()
+    else:
+        # Использование
+        run_with_saved_weights("best_network_weights.json")
